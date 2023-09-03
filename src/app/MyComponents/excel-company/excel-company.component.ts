ec5116ba88b6f46bec60860179df277c66470aa0
janettrez7 <treasahelosia7@gmail.com>
2023-09-04 01:05:13 +0530

    commit1

---
+++ src/app/MyComponents/excel-company/excel-company.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import {Com}
+@Component({
+  selector: 'app-excel-company',
+  templateUrl: './excel-company.component.html',
+  styleUrls: ['./excel-company.component.css']
+})
+export class ExcelCompanyComponent {
+
+}
